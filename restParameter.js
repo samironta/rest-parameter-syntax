04696984a09e8ae82b4fd2e8ab06f21ec0231fa6
samironta <samironta@outlook.com>
2022-02-09 15:42:01 +0000

    changed parameters on the sort and function call

--- restParameter.js
+++ restParameter.js
@@ -29,11 +29,11 @@ let mixedLetters = ['b', 'd', 'a', 'c', 'f', 'e'];
 let moreMixedLetters = [...mixedLetters, 'h', 'k', 'g', 'j', 'i', 'l'];
 console.log(moreMixedLetters);
 const updateSortReverse = (arr, ...letters) => {
-    let sortReverse = [arr, ...letters].sort();
+    let sortReverse = [...arr, ...letters].sort();
     sortReverse = sortReverse.reverse();
     return sortReverse;
 
 }
-let reverseSort = updateSortReverse(...moreMixedLetters, 'n', 'm', 'o');
+let reverseSort = updateSortReverse(moreMixedLetters, 'n', 'm', 'o');
 console.log(reverseSort);
 console.log(mixedLetters);
